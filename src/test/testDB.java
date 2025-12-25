8cfc8aa90fa04c8449a92bf029a5bef55c878862
sethartykun <sethartykun16@gmail.com>
2025-12-25 21:58:47 +0700

    add dbconnection and test

---
+++ src/test/testDB.java
@@ -0,0 +1,14 @@
+import db.DatabaseConnection;
+import java.sql.Connection;
+
+public class testDB {
+    public static void main(String[] args){
+        Connection conn = DatabaseConnection.getConnection();
+
+        if (conn != null) {
+            System.out.println("✅ Connected to MySQL successfully!");
+        } else {
+            System.out.println("❌ Connection failed!");
+        }
+    }
+}
